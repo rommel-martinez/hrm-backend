6b6e53641e5d756269dfc7e26c687c8ecd556c9c
rommelvmartinez@gmail.com <rommelvmartinez@gmail.com>
2024-06-04 16:12:19 -0300

    SOME TEXT

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const conn = require('../database/connection')
 var moment = require('moment');
+const connection = require('../database/connection');
 
 router.post(`/create`, async (req, res) => {
     const { First_Name, 
@@ -103,5 +104,85 @@ router.get(`/getemployee`, async (req, res) => {
     }
 })
 
+router.post(`/update`, async (req, res) => {
+    const { Id,
+            First_Name, 
+            Last_Name, 
+            Nick_Name, 
+            Gender,
+            Marital_Status, 
+            Email, 
+            Mobile,
+            Photo,
+            DOB,
+            Address,
+            City,
+            Province,
+            Country,
+            Post_Code,
+            Status,
+            Active,
+            Last_Changed_By
+            } = req.body
 
-module.exports = router
+    try {
+        const query = `UPDATE employees ` +
+                      `SET ` +
+                        `First_Name = '${First_Name}', ` +
+                        `Last_Name = '${Last_Name}', ` + 
+                        `Nick_Name = '${Nick_Name}', ` + 
+                        `Gender = '${Gender}', ` +
+                        `Marital_Status = '${Marital_Status}', ` + 
+                        `Email = '${Email}', ` + 
+                        `Mobile = '${Mobile}', ` +
+                        `Photo = '${Photo}', ` +
+                        `DOB = '${DOB}', ` +
+                        `Address = '${Address}', ` +
+                        `City = '${City}', ` +
+                        `Province = '${Province}', ` +
+                        `Country = '${Country}', ` +
+                        `Post_Code = '${Post_Code}', ` +
+                        `Status = '${Status}', ` +
+                        `Active = '${Active}', ` +
+                        `Last_Changed_By = '${Last_Changed_By}', ` +
+                      `WHERE Id = ${Id}`
+
+        const result = connection(query);
+
+        if(result){
+            res.status(200).send(result)
+        } else {
+            res.status(500).send({
+                Status: result,
+                message: result.message
+            })
+        }
+
+    } catch (error) {
+        return false
+    }
+})
+
+router.post(`/delete`, async (req, res) => {
+    const { id } = req.query
+
+    try {
+        const query = `DELETE FROM employee WHERE id = ${id}`
+
+        const result = connection(query)
+
+        if(result){
+            res.status(200).send(result)
+        } else {
+            res.status(500).send({
+                status: result,
+                message: result.message
+            })
+        }
+
+    } catch (error) {
+        return false
+    }
+})
+
+module.exports = router 
